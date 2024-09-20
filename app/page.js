2f680a445f66d75e7bd37a78199187f4c104930a
liamjwoods5 <liam.woods@edu.sait.ca>
2024-09-20 10:40:13 -0600

    Week 2 Assignment work
    finished week 2 assignment and pushed

--- app/page.js
+++ app/page.js
@@ -2,6 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  
+    <div>
+      <h1 className="text-xl m-5 mb-1">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <ul className="ml-7">
+        <li>
+          <a href="/week-2" className="hover:text-blue-500 hover:underline">
+            Week-2 Assignment
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Liam Woods</h1>
+      <a
+        href="https://github.com/liamjwoods5/cprg306-assignments.git"
+        className="hover:text-blue-600 hover:underline"
+      >
+        Github Repository
+      </a>
+    </div>
+  );
+}
